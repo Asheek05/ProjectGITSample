58616c47af5e296c2bc7977fe6c3384cfc953f51
Asheek05 <ashik.rocket@gmail.com>
2022-08-19 17:37:39 +0530

    Added ClassSample1

---
+++ src/test/java/com/samp/ClassSample1.java
@@ -0,0 +1,15 @@
+package com.samp;
+
+public class ClassSample1 {
+	
+	public void companyName() {
+		System.out.println("Greens");
+	}
+	private void companyLocation() {
+		System.out.println("Chennai");
+	}
+	private void companyHead() {
+		System.out.println("Kolkatta");
+	}
+
+}
